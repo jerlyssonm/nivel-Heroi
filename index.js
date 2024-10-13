45d77c16d0f304d5abf6817138439a68e9e4ce27
jerlyssonm <95264940+jerlyssonm@users.noreply.github.com>
2024-10-13 00:04:38 -0300

    finalizando desafio do nivelheroi

---
+++ index.js
@@ -0,0 +1,27 @@
+let nickName = "Super Green"
+let xp = 20011
+
+if (xp <= 1000 ){
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Ferro**`)
+}
+else if (xp <= 2000 ){
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Bronze**`)
+}
+else if (xp <= 5000 ){
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Prata**`)
+}
+else if (xp <= 7000 ){
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Ouro**`)
+}
+else if (xp <= 8000 ){
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Platina**`)
+}
+else if (xp <= 9000 ){
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Ascendente**`)
+}
+else if (xp <= 10000 ){
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Imortal**`)
+}
+else{
+    console.log(`O Heroi de nome **${nickName}** está no nível de **Radiante**`)
+}
